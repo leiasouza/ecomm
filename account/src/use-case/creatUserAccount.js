5c2a73e2879d552bf4bf4cab9fb56064827f3265
Vanderleia Rodrigues de Suza <leia.souzarodrigues@hotmail.com>
2022-11-23 14:56:31 -0300

    criando conta de usuário

---
+++ account/src/use-case/creatUserAccount.js
@@ -0,0 +1,19 @@
+let contas = []
+
+export function creatUserUseCase(nome, email, senha) {
+    
+const user = {
+    id: contas.length + 1,
+    name: nome,
+    email: email,
+    password: senha,
+    createdData: new Date()
+
+}
+    contas.push(user)
+    return user
+}
+
+creatUserUseCase("Josué Lima", 'josuelima@email', 'senhaDoJosue')
+creatUserUseCase("Maria Lima", 'marialima@email', 'senhaDaMaria')
+
