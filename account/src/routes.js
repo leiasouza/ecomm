2e23a5881cc553eb6aac906ccd750ef21e5649ba
SOUZA VANDERLEIA RODRIGUES DE <n863108@gruposantander.com>
2022-12-15 16:15:11 -0300

    retirando senha usuário

--- account/src/routes.js
+++ account/src/routes.js
@@ -9,6 +9,7 @@ router.post('/accounts', function(request, response) {
     const { name, email, password } = request.body;
     createUserUseCase(name, email, password)
         .then(createUserUseCase => {
+            delete createUserUseCase.password
             response.status(201).json(createUserUseCase)
         })
         .catch(error => {
