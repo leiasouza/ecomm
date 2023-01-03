add96f81f0216abd306c566f2a2f95f27eb0f081
SOUZA VANDERLEIA RODRIGUES DE <n863108@gruposantander.com>
2023-01-03 12:46:31 -0300

    refatorando

--- account/src/routes.js
+++ account/src/routes.js
@@ -11,7 +11,7 @@ router.post('/accounts', async (request, response) => {
         id: createdUser._id,
         name: createdUser.name,
         email: createdUser.email,
-        createdData: createdUser.createdData, 
+        createdDate: createdUser.createdData, 
     }); 
 });
 
