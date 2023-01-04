d7a2c224fd0a96f4becfbf153395b9f23031681a
SOUZA VANDERLEIA RODRIGUES DE <n863108@gruposantander.com>
2023-01-04 11:30:25 -0300

    alterando documentação de json para yaml

--- account/src/main.js
+++ account/src/main.js
@@ -1,23 +1,20 @@
-import express from "express";
-import { router } from "./routes.js";
-import swaggerUI from 'swagger-ui-express';
-import swaggerDocument from "../api-docs.json" assert {type: "json"};
-import cors from 'cors';
+import express from 'express';
+import yamljs from 'yamljs';
+import swaggerExpress from 'swagger-ui-express';
+import { router } from './routes.js';
 
-
+const swaggerDocs = yamljs.load('./docs.yaml');
 const app = express();
-
 app.use(express.json());
-app.use(cors());
 
-app.get('/healt', (request, response) => {
-   return response.send();
+app.get('/health', (request, response) => {
+    return response.send();
 });
 
 app.use(router);
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use('/docs', swaggerExpress.serve, swaggerExpress.setup(swaggerDocs))
 
-app.listen(3001, () => {
-    console.log('accounts service is running');  
+app.listen(3000, () => {
+    console.log('accounts service is running');
 });
--- product/src/main.js
+++ product/src/main.js
@@ -1,20 +1,20 @@
 import express  from "express";
 import { router } from "./routes.js";
-import swaggerUI from 'swagger-ui-express';
-import swaggerDocument from "../api-docs.json" assert {type: "json"};
-import cors from 'cors';
+import swaggerExpress from 'swagger-ui-express';
+import yaml from 'yamljs';
+
+const swaggerDocs = yaml.load('./docs.yaml');
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
 app.get('/health', (request, response) =>{
     return response.send();
 });
 
 app.use(router);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use('/docs', swaggerExpress.serve, swaggerExpress.setup(swaggerDocs));
 
 app.listen(3000, () => {
     console.log('products service is running');  
