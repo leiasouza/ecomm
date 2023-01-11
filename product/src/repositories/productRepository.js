6cf038ec3b8c7cfa3547a70dde6f250f0a85cb4e
SOUZA VANDERLEIA RODRIGUES DE <n863108@gruposantander.com>
2023-01-11 15:58:33 -0300

    Criando tabelas de imagens e características e criando relacionamentos entre as tabelas

--- product/src/repositories/productRepository.js
+++ product/src/repositories/productRepository.js
@@ -1,15 +1,27 @@
 import { Product } from '../../models/product.js';
+import { ProductFeture } from '../../models/product_feture.js';
+import { ProductImage } from '../../models/product_image.js';
+
 export async function saveProduct(produto) {
-    const createdProduct = await Product.create(produto);
+    const createdProduct = await Product.create(produto, {
+        include: [
+            { association: Product.ProductFeture, as: 'features',},
+            { association: Product.ProductImage, as: 'images'}
+        ]
+    });
     await createdProduct.save();
     return createdProduct;
-    
-
-
-    
 }
 
 export async function findProducts() {
-    const allProducts = await Product.findAll();
+    const allProducts = await Product.findAll({ include : [
+        {
+            model: ProductFeture,
+            as: 'fetures'
+        }, {
+            model: ProductImage,
+            as: 'images'
+        }]
+    });
     return allProducts;
 }
