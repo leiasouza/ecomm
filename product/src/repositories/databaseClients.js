faff78558e4fb85943a606473e5b345b93d75beb
SOUZA VANDERLEIA RODRIGUES DE <n863108@gruposantander.com>
2023-01-17 00:18:07 -0300

    testando enpoints product

--- product/src/repositories/databaseClients.js
+++ product/src/repositories/databaseClients.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize';
 
-const client = new Sequelize('mysql://vanderleia:23456@product_db:3306/products');
+const client = new Sequelize(process.env.DATABASE_URL);
 
 export default client;
