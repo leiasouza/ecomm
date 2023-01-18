530bd108d2113513c8fa244fb680134bab6ffb42
SOUZA VANDERLEIA RODRIGUES DE <n863108@gruposantander.com>
2023-01-18 17:34:10 -0300

    teste endpoints de products

--- product/src/test/e2e/listProduct.test.js
+++ product/src/test/e2e/listProduct.test.js
@@ -2,21 +2,11 @@ import { app } from "../../app.js";
 import request from "supertest";
 
 describe("Lista produtos", () => {
-  it("Lista de produtos vazia", async () => {
-    await request(app)
-      .get("/products")
-      .expect(200)
-      .expect(({ body }) => {
-        expect(body).toEqual([]);
-      });
-  });
-
   it("Lista de produtos cadastrados", async () => {
     await request(app)
       .get("/products")
-      .expect(200)
-      .expect(({ body }) => {
-        expect(body).toEqual([]);
+      .then((response) => {
+        expect(response.status).toEqual(200);
       });
   });
 });
