f35c605c16b12997b3f82b7c33f6723e1c88072f
SOUZA VANDERLEIA RODRIGUES DE <n863108@gruposantander.com>
2023-01-21 01:05:04 -0300

    iniciando autenticação de um usuário

--- account/src/app.js
+++ account/src/app.js
@@ -1,9 +1,25 @@
 import express  from "express";
 import { router } from "./routes.js";
+import jwt from 'jsonwebtoken'
+import  compare from 'bcryptjs';
+
 
 const app = express();
 
 app.use(express.json());
+
+
+app.get('/user', (request, response) =>{
+    return response.json([{_id: 1, name: 'Alice'}]);
+
+});
+app.post('/login', async (request, response) => {
+    if(request.body.user === 'Alice'  && request.body.password === 'SenhaDaAlice') {
+       const token = jwt.sign({ _id: 1 }, compare, {expiresIn: 14400 });
+        return response.json(201);
+    }
+    response.status(400).end();
+    }); 
 
 app.get('/health', (request, response) =>{
     return response.send();
--- account/src/repositories/accountRepository.js
+++ account/src/repositories/accountRepository.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-export const client = new MongoClient('mongodb://vanderleia:123456@localhost:27017');
+export const client = new MongoClient(process.env.DATABASE_URL);
 
 export async function getUsersCollection(client) {
     const database = client.db('accounts');
@@ -14,4 +14,23 @@ export async function saveAccount(account) {
     await usersCollection.insertOne(account);
     await client.close();
     
+}
+
+export async function listAccounts() {
+    const userCollection = await databaseConnect();
+    const accounts = userCollection.find().toArray();
+
+    return accounts;
+}
+
+export async function findAccountById(id) {
+    const userCollection = await databaseConnect();
+    const account = userCollection.findOne({ _id: ObjectId(id) });
+    return account;
+}
+
+export async function existsAccountById(id) {
+    const account = await findAccountById(id);
+
+    return account !== null;
 }
