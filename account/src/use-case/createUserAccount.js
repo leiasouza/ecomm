90c90ba2d609e5fcca06ed6681b60b6036c7758a
SOUZA VANDERLEIA RODRIGUES DE <n863108@gruposantander.com>
2023-01-26 00:33:12 -0300

    evitando user duplicados na base de dados

--- account/src/use-case/createUserAccount.js
+++ account/src/use-case/createUserAccount.js
@@ -1,7 +1,16 @@
 import  bcrypt from 'bcryptjs';
-import { saveAccount } from "../repositories/accountRepository.js";
+import { findUserByEmail, saveAccount } from "../repositories/accountRepository.js";
+
 
 export async function createUserUseCase(name, email, password) {
+
+    const userAlreadyExists = await findUserByEmail(email);
+    if(userAlreadyExists) {
+        console.error('e-mail already registered', email);
+        return;
+
+    }
+
     const createdDate = new Date().toISOString().substring(0, 10);
     const passwordHash = bcrypt.hashSync(password ,10);
     const user = {
