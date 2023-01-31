bdf8c3f4797b6bc80c39c9d379020c01d4e71b71
SOUZA VANDERLEIA RODRIGUES DE <n863108@gruposantander.com>
2023-01-30 23:29:07 -0300

    adicionando testes

--- product/src/test/e2e/createProduct.test.js
+++ product/src/test/e2e/createProduct.test.js
@@ -1,19 +1,27 @@
 import { productExample } from "../data/products.js";
 import { app } from "../../app.js";
 import request from "supertest";
+import { generateToken } from "../data/helpers/tokens.js";
+import { cleanProductTable } from "../data/helpers/products.js";
 
 describe("Criação de produto", () => {
+  afterEach(async () => {
+    await cleanProductTable();
+  });
+
   it("cria novo produto", async () => {
     await request(app)
       .post("/products")
       .set("Content-Type", "application/json")
       .set("Accept", "aplication/json")
+      .set("Authorization", `Bearer ${generateToken("id-do-usuario")}`)
       .send(productExample)
       .expect(201)
       .expect(({ body }) => {
         expect(body).toEqual({
           ...productExample,
           id: expect.any(Number),
+          user_id: "id-do-usuario",
           createdAt: expect.any(String),
           updatedAt: expect.any(String),
           fetures: productExample.fetures.map((feture) => ({
@@ -29,8 +37,41 @@ describe("Criação de produto", () => {
             product_id: body.id,
             createdAt: expect.any(String),
             updatedAt: expect.any(String),
-          }))
+          })),
         });
+      });
+  });
+  it("should not create a product when no authorization info is provided", async () => {
+    await request(app)
+      .post("/products")
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
+      .expect(401)
+      .expect(({ body }) => {
+        expect(body).toEqual({ message: "authentication required" });
+      });
+  });
+  it("should not create a product when authorization info is malformed", async () => {
+    await request(app)
+      .post("/products")
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
+      .set("Authorization", "header-errado")
+      .expect(400)
+      .expect(({ body }) => {
+        expect(body).toEqual({ message: "authorization header malformed" });
+      });
+  });
+  it("should not create a product when authorization info was modified", async () => {
+    const modifiedToken = generateToken("id-usuario") + "a";
+    await request(app)
+      .post("/products")
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
+      .set("Authorization", `Bearer ${modifiedToken}`)
+      .expect(403)
+      .expect(({ body }) => {
+        expect(body).toEqual({ message: "forbidden" });
       });
   });
 });
--- product/src/test/e2e/listProduct.test.js
+++ product/src/test/e2e/listProduct.test.js
@@ -2,11 +2,11 @@ import { app } from "../../app.js";
 import request from "supertest";
 import { productExample } from "../data/products.js";
 import { saveProduct } from "../../repositories/productRepository.js";
-import { clearProductTable } from "../data/helpers/products.js";
+import { cleanProductTable } from "../data/helpers/products.js";
 
 describe("Lista produtos", () => {
   afterEach(async () => {
-    clearProductTable;
+    cleanProductTable;
   });
   it("Lista de produtos vazia", async () => {
     await request(app)
@@ -18,34 +18,43 @@ describe("Lista produtos", () => {
   });
 
   it("Lista de produtos cadastrados", async () => {
-    await saveProduct(productExample)
+    await saveProduct({ ...productExample, user_id: "id-do-usuario" });
     await request(app)
       .get("/products")
       .expect(200)
       .expect(({ body }) => {
         expect(body.length).toBe(1);
-        expect(body).toEqual(expect.arrayContaining([{
-          ...productExample,
-          value: String(productExample.value),
-          id: expect.any(Number),
-          createdAt: expect.any(String),
-          updatedAt: expect.any(String),
-          fetures: expect.arrayContaining(productExample.fetures.map(feture => ({
-              ...feture,
+        expect(body).toEqual(
+          expect.arrayContaining([
+            {
+              ...productExample,
+              user_id: "id-do-usuario",
+              value: String(productExample.value),
               id: expect.any(Number),
-              product_id: body[0].id,
               createdAt: expect.any(String),
               updatedAt: expect.any(String),
-            }))),
+              fetures: expect.arrayContaining(
+                productExample.fetures.map((feture) => ({
+                  ...feture,
+                  id: expect.any(Number),
+                  product_id: body[0].id,
+                  createdAt: expect.any(String),
+                  updatedAt: expect.any(String),
+                }))
+              ),
 
-          images: expect.arrayContaining(productExample.images.map(image => ({
-              ...image,
-              id: expect.any(Number),
-              product_id: body[0].id,
-              createdAt: expect.any(String),
-              updatedAt: expect.any(String),
-            }))),
-        }]));
+              images: expect.arrayContaining(
+                productExample.images.map((image) => ({
+                  ...image,
+                  id: expect.any(Number),
+                  product_id: body[0].id,
+                  createdAt: expect.any(String),
+                  updatedAt: expect.any(String),
+                }))
+              ),
+            },
+          ])
+        );
       });
   });
 });
