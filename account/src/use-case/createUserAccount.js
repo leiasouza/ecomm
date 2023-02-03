9aa485723d9066379861bc2b61e120e0dd5970ce
SOUZA VANDERLEIA RODRIGUES DE <n863108@gruposantander.com>
2023-02-03 19:42:36 -0300

    adicionando schema de validação

--- account/src/use-case/createUserAccount.js
+++ account/src/use-case/createUserAccount.js
@@ -3,8 +3,27 @@ import {
   findUserByEmail,
   saveAccount,
 } from "../repositories/accountRepository.js";
+import joi from "joi";
+
+const accountValidator = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().min(8).required(),
+});
 
 export async function createUserUseCase(name, email, password) {
+  const { error } = accountValidator.validate({ email, password });
+
+  if (error) {
+    return {
+      hasError: true,
+      errors: error.details.map((error) => ({
+        message: error.message,
+        property: error.path.at(0),
+      })),
+      account: {},
+    };
+  }
+
   const userAlreadyExists = await findUserByEmail(email);
 
   if (userAlreadyExists) {
@@ -12,13 +31,21 @@ export async function createUserUseCase(name, email, password) {
   }
 
   const createdDate = new Date().toISOString().substring(0, 10);
-  const passwordHash = bcrypt.hashSync(password, process.env.PASSWORD_SALT| Number );
+  const passwordHash = bcrypt.hashSync(
+    password,
+    process.env.PASSWORD_SALT | Number
+  );
   const user = {
     name,
     email,
     password: passwordHash,
     createdDate,
   };
+
   await saveAccount(user);
-  return user;
+  return {
+    hasError: false,
+    error: undefined,
+    account: {...user, password: undefined }
+  };
 }
