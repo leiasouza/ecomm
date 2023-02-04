e63201e29b87be2f42103fe0b24f112ea68ceef6
SOUZA VANDERLEIA RODRIGUES DE <n863108@gruposantander.com>
2023-02-04 00:23:47 -0300

    Validando produtos

--- product/src/usecase/createProductUseCase.js
+++ product/src/usecase/createProductUseCase.js
@@ -1,6 +1,48 @@
 import { saveProduct } from "../repositories/productRepository.js";
+import {joi} from 'joi';
 
-export async function createProductUseCase(produto, userId) {
-    const savedProduct = await saveProduct({...produto, user_id: userId});
-    return savedProduct;
+const productValidator = joi.object({
+    name: joi.string().trim().required(),
+    user_id: joi.string().trim().required().guid({ version: ['uuidv4']}),
+    value: joi.number().min(0.5).required(),
+    quantity: joi.number().min(1).required(),
+    description: joi.string().trim().min(200).max(500).required(),
+    category: joi.string().trim().required(),
+    features: joi.array().min(3).required().items(
+        joi.object({
+            name: joi.string().trim().required(),
+            description: joi.string().trim().required(),
+        })
+    ),
+    images: joi.array().min(3).required().items(
+        joi.object({
+            url: joi.string().trim().required().uri({ scheme: [/https?/] }),
+            description: joi.string().trim().required(),
+        })
+    ),
+})
+
+
+export async function createProductUseCase(product, userId) {
+    const productToSave = {...product, user_id: userId };
+
+    const { error } = productValidator.validate(productToSave, { abortEarly: false });
+
+    if(error) {
+       return { 
+            hasErrors: true, 
+            errors: error.details.map(error => ({
+                message: error.message,
+                property: error.path.at(0),
+            })), 
+            product: productToSave,
+        }
+    }
+    const savedProduct = await saveProduct(productToSave);
+
+    return {
+        hasErrors: false,
+        errors: undefined,
+        product: savedProduct
+    };
 }
